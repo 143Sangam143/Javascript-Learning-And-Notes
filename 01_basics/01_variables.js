137e81fce2a2502de20e72c73c6fe26d906d8f19
143Sangam143 <108720249+143Sangam143@users.noreply.github.com>
2023-11-11 04:29:38 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "sangam@gmail.com"
+var accountPassword = "12345"
+accountCity = "Manthali"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "katwal"
+accountPassword = "21212121"
+accountCity = 'kathamndu'
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";  // treat all JS code as newer version
+
+// alert('hello') //we are using nodejs, not browser
+
+console.log(3+3)
+
+console.log("Sangam");
+
+//code readability should be high
+
+let name = "Sangam"
+let age = 25
+let isLoggedIn = false
+
+// number 
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol
+
+//object
+
+console.log(typeof 'sangam');
