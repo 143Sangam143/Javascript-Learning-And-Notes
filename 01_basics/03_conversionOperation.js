760b6397d1449f7a925c8a0c0a58bdb41fa631f7
143Sangam143 <108720249+143Sangam143@users.noreply.github.com>
2023-11-23 03:44:23 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,12 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNUmber = Number(score);
+// console.log(valueInNUmber);
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
