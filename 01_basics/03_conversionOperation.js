1cca79e3d3ea0b3494e3aab60a1d2335b1cffb33
143Sangam143 <108720249+143Sangam143@users.noreply.github.com>
2023-11-23 04:00:44 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -9,4 +9,15 @@ let valueInNUmber = Number(score);
 let isLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+/*************** Operations *************************/
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "helow"
+let str2 = " world"
+let str3 = str1 + str2
+// console.log(str3);
+
